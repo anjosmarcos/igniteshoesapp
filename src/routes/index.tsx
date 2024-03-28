fd3d63b7499f944ee292859c65c9347ea64fa828
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-28 10:54:55 -0300

    Configurando navegação do Deep Linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,18 @@ import { useEffect, useState } from 'react';
 import { NotificationWillDisplayEvent, OSNotification, OneSignal } from 'react-native-onesignal';
 import { Notification } from '../components/Notification';
 
+const linking = {
+  prefixes: ['com.rocketseat.igniteshoes://'],
+  config: {
+    screens: {
+      details: '/details/:productId',
+      parse: {
+        productId: (productId: string) => productId,
+      }
+    },
+  },
+}
+
 export function Routes() {
   const { colors } = useTheme();
   const [notification, setNotification] = useState<OSNotification>();
@@ -26,7 +38,7 @@ export function Routes() {
   }, [])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {
