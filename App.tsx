c245ab0624a8c79b0ac52a9b880993c0cb813520
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-03-28 10:21:17 -0300

    Obtendo a opção selecionada

--- App.tsx
+++ App.tsx
@@ -23,7 +23,19 @@ export default function App() {
 
   useEffect(() => {
     const handleNotificationClick = (event: NotificationClickEvent) => {
-      console.log(event)
+      const { actionId } = event.result
+    
+      switch(actionId) {
+        case "1":
+          console.log('ver todos')
+          break
+        case "2":
+          console.log("Ver pedido")
+          break
+        default: 
+          console.log('Nenhuma ação')
+      }
+
     }
 
     OneSignal.Notifications.addEventListener("click", handleNotificationClick)
